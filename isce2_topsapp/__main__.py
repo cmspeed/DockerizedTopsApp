513e73fef81d4136e8c91b715c8bc4ef91b96496
cmspeed <cole.speed@jpl.nasa.gov>
2025-02-20 16:18:03 -0800

    Extract conversion functions from __main__.py

--- isce2_topsapp/__main__.py
+++ isce2_topsapp/__main__.py
@@ -1,15 +1,10 @@
 import json
 import math
 import netrc
-import numpy as np
 import os
 import sys
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
 from importlib.metadata import entry_points
-import isce
-from iscesys.Component.ProductManager import ProductManager
-from isceobj.Planet.Planet import Planet
-from osgeo import gdal
 from pathlib import Path
 from platform import system
 from typing import Optional
@@ -28,6 +23,8 @@ from isce2_topsapp import (BurstParams,
                            prepare_for_delivery,
                            topsappParams,
                            topsapp_processing,
+                           convert_offsets,
+                           convert_unwrapped,
                            create_viz_files
                            )
 from isce2_topsapp.iono_proc import iono_processing
@@ -216,162 +213,6 @@ def update_slc_namespace(args: Namespace) -> Namespace:
         raise ValueError('Goldstein filter power must be non-negative')
 
     return args
-
-
-def load_product(xml_name):
-        """Load the product using Product Manager."""
-        pm = ProductManager()
-        pm.configure()
-        return pm.loadProduct(xml_name)
-
-
-def get_conversion_factors(reference_xml):
-    obj = load_product(reference_xml)
-    burst = obj.bursts[0]
-    rangePixelSize = burst.rangePixelSize
-
-    # Get azimuthPixelSize from satellite speed and height
-    azimuthTimeInterval = burst.azimuthTimeInterval
-    sensingMid = burst.sensingMid
-    orb = obj.orbit
-    Vs = np.linalg.norm(orb.interpolateOrbit(sensingMid,method='hermite').getVelocity())  #satellite velocity at center
-    Ps_vec = orb.interpolateOrbit(sensingMid,method='hermite').getPosition()
-    Ps = np.linalg.norm(Ps_vec)  #satellite position at center
-
-    # Approximate terrain height 
-    terrainHeight = burst.terrainHeight
-
-    #latitude, longitude, elevation at image center
-    midRange = burst.midRange
-    llh_cen = orb.rdr2geo(sensingMid,midRange,height=terrainHeight)
-
-    refElp = Planet(pname='Earth'). ellipsoid
-    xyz_cen = refElp.llh_to_xyz(llh_cen) #xyz coordinate at image center
-
-    Re = np.linalg.norm(xyz_cen)
-    cosb = (Ps**2 + Re**2 - midRange**2)/(2*Ps*Re)
-    Vg = (Re*cosb)*Vs/Ps
-
-    azimuthPixelSize = azimuthTimeInterval*Vg
-
-    print('satellite velocity (m/s)',Vs)
-    print('satellite velocity over the ground (m/s)',Vg)    
-    print('rangePixelSize (m)',rangePixelSize)
-    print('azimuthPixelSize (m)',azimuthPixelSize)
-    return azimuthPixelSize, rangePixelSize
-
-
-def convert_offsets(dense_offsets, amplitude, reference_xml):
-    azimuthPixelSize, rangePixelSize = get_conversion_factors(reference_xml)
-
-    # Open the input raster
-    dense_offsets_pix = gdal.Open(dense_offsets)
-    if not dense_offsets_pix:
-        raise RuntimeError(f"Failed to open file: {dense_offsets_pix}")
-
-    # Open amplitude which will be used for masking
-    amplitude_pix = gdal.Open(amplitude)
-    if not amplitude_pix:
-        raise RuntimeError(f"Failed to open file: {amplitude}")
-    amp = amplitude_pix.GetRasterBand(1).ReadAsArray()
-    amp_NoData = amplitude_pix.GetRasterBand(1).GetNoDataValue() 
-    if amp_NoData is None:
-        amp_NoData = 0  # Default to 0 if NoData value is not explicitly set
-    
-    # NoData value for the output raster
-    nodata_value = -10000
-
-    # Read the first band (azimuth) and convert, set nodata value where amplitude is nodata
-    azimuth_pix = dense_offsets_pix.GetRasterBand(1).ReadAsArray()
-    azimuth_m = np.where(
-        (amp == amp_NoData) | (azimuth_pix == nodata_value),
-        nodata_value,
-        azimuth_pix * azimuthPixelSize
-    )
-
-    # Read the second band (range) and convert
-    range_pix = dense_offsets_pix.GetRasterBand(2).ReadAsArray()
-    range_m = np.where(
-        (amp == amp_NoData) | (range_pix == nodata_value),
-        nodata_value,
-        range_pix * rangePixelSize
-    )
-
-    # Write the converted arrays to a new raster
-    driver = gdal.GetDriverByName("ISCE")
-    outfile = "merged/filt_dense_offsets_m.geo"
-
-    # Create the new raster
-    dense_offsets_m = driver.Create(outfile, dense_offsets_pix.RasterXSize, dense_offsets_pix.RasterYSize, 2, gdal.GDT_Float32)
-
-    # Get the raster bands
-    band1 = dense_offsets_m.GetRasterBand(1)
-    band2 = dense_offsets_m.GetRasterBand(2)
-
-    # Set the NoData value for both bands before writing data
-    band1.SetNoDataValue(nodata_value)
-    band2.SetNoDataValue(nodata_value)
-
-    # Write the azimuth_m array to band 1 and range_m array to band 2
-    band1.WriteArray(azimuth_m)
-    band2.WriteArray(range_m)
-
-    # Flush data to disk
-    band1.FlushCache()
-    band2.FlushCache()
-
-    # Ensure georeferencing information is same as input
-    dense_offsets_m.SetGeoTransform(dense_offsets_pix.GetGeoTransform())
-    dense_offsets_m.SetProjection(dense_offsets_pix.GetProjection())
-
-    return
-
-
-def convert_unwrapped(unwrapped_pix):
-    # Open the input raster
-    unwrapped = gdal.Open(unwrapped_pix)
-
-    # Read the amplitude and phase arrays
-    amp_array = unwrapped.GetRasterBand(1).ReadAsArray()
-    phase_array = unwrapped.GetRasterBand(2).ReadAsArray()
-
-    # Convert phase to meters
-    phase_m = (phase_array * 0.05546576)/(4 * np.pi)
-   
-    # Write the converted arrays to a new raster
-    driver = gdal.GetDriverByName("ISCE")
-    outfile = "merged/filt_topophase_m.unw.geo"
-    vrtfile = "merged/filt_topophase_m.unw.geo.vrt"
-   
-    # Create the new raster
-    unwrapped_m = driver.Create(outfile, unwrapped.RasterXSize, unwrapped.RasterYSize, 2, gdal.GDT_Float32)
-   
-    # Get the raster bands
-    band1 = unwrapped_m.GetRasterBand(1)
-    band2 = unwrapped_m.GetRasterBand(2)
-   
-    # Set the NoData value for both bands before writing data
-    band1.SetNoDataValue(0)
-    band2.SetNoDataValue(0)
-   
-    # Write the amp_array to band 1 and phase_m to band 2
-    band1.WriteArray(amp_array)
-    band2.WriteArray(phase_m)
-
-    # Ensure georeferencing information is same as input
-    unwrapped_m.SetGeoTransform(unwrapped.GetGeoTransform())
-    unwrapped_m.SetProjection(unwrapped.GetProjection())
-
-    # Flush data to disk
-    band1.FlushCache()
-    band2.FlushCache()
-    unwrapped_m.FlushCache()
-    unwrapped_m = None 
-   
-    # Create a .vrt file
-    gdal.BuildVRT(vrtfile, [outfile])
-
-    return
    
 
 def gunw_slc():
@@ -447,7 +288,7 @@ def gunw_slc():
     convert_offsets('merged/filt_dense_offsets.bil.geo', 'merged/filt_topophase.unw.geo', 'reference/IW2.xml')
 
     # Convert unwrapped phase to meters
-    convert_unwrapped('merged/filt_topophase.unw.geo')
+    convert_unwrapped('merged/filt_topophase.unw.geo', "merged/filt_topophase_m.unw.geo", "merged/filt_topophase_m.unw.geo.vrt")
 
     additional_2d_layers_for_packaging = []
     additional_attributes_for_packaging = {}
@@ -516,6 +357,7 @@ def gunw_slc():
         create_viz_files(nc_file_path, cogs_dir, tiles_dir, footprint_dir, water_mask_path)
     except Exception as e:
         print(f'Error creating visualization files: {e}')
+
 
 def gunw_burst():
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
